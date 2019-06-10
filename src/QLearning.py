Fixed average length calculation

src/QLearning.py
@@ -33,7 +33,7 @@ def evaluateScore(Q, boardDim, numRuns, displayGame):
             action = np.argmax(possibleQs)
             state, reward, gameOver, score = game.makeMove(action)
             if displayGame:
-                clear()
+                #                clear()
                 game.display()
                 print("Moves without increasing score:", moveCounter)
                 print("Snake Length:", score)
@@ -46,10 +46,9 @@ def evaluateScore(Q, boardDim, numRuns, displayGame):
                 moveCounter = 0
             if moveCounter >= cutoff:
                 # stuck going back and forth
-                scores.append(score)
                 break
-
-    return np.average(score)
+        scores.append(score)
+    return np.average(scores), scores
 
 
 # %%
@@ -65,11 +64,12 @@ numActions = 4  # 4 directions that the snake can move
 Q = np.zeros((numStates, numActions))
 
 # lr = 0.9 #learning rate. not used in this Q learning equation
-gamma = 0.9  # discount rate
+gamma = 0.8  # discount rate
 epsilon = 0.2  # exploration rate in training games
 numEpochs = 5000  # number of games to train for
 
 Qs = []
+bestLength = 0
 print("Training for", numEpochs, "games...")
 for epoch in range(numEpochs):
     #    print("New Game")
@@ -92,9 +92,16 @@ for epoch in range(numEpochs):
         # Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) - Q[state, action])
         state = new_state
     if epoch % 100 == 0:
-        print("Epoch", epoch, "Average snake length without exploration:", evaluateScore(Q, boardDim, 25, False))
+        averageLength, lengths = evaluateScore(Q, boardDim, 50, False)
+        if averageLength > bestLength:
+            bestLength = averageLength
+            bestQ = np.copy(Q)
+        print("Epoch", epoch, "Average snake length without exploration:", averageLength)
 
 # %%
 
-print("Testing with last trained Q matrix...")
-print("Average snake length:", evaluateScore(Q, boardDim, 5, True))
+
+print("Testing with best trained Q matrix...")
+averageLength, lengths = evaluateScore(bestQ, boardDim, 5, True)
+print("Average snake length:", averageLength)
+print("Lengths:", lengths)

src/Snake.py
@@ -11,24 +11,25 @@ class BodyNode():
         self.parent = parent
         self.x = x
         self.y = y
-        
+
     def setX(self, x):
         self.x = x
-    
+
     def setY(self, y):
         self.y = y
-        
+
     def setParent(self, parent):
         self.parent = parent
-    
+
     def getPosition(self):
         return (self.x, self.y)
-        
+
+
 class Snake():
     def __init__(self, x, y):
         self.head = BodyNode(None, x, y)
         self.tail = self.head
-        
+
     def moveBodyForwards(self):
         currentNode = self.tail
         while currentNode.parent != None:
@@ -36,7 +37,7 @@ class Snake():
             currentNode.setX(parentPosition[0])
             currentNode.setY(parentPosition[1])
             currentNode = currentNode.parent
-        
+
     def move(self, direction):
         (oldTailX, oldTailY) = self.tail.getPosition()
         self.moveBodyForwards()
@@ -50,62 +51,63 @@ class Snake():
         elif direction == 3:
             self.head.setX(headPosition[0] - 1)
         return (oldTailX, oldTailY, *self.head.getPosition())
-    
+
     def newHead(self, newX, newY):
         newHead = BodyNode(None, newX, newY)
         self.head.setParent(newHead)
         self.head = newHead
-        
+
     def getHeadPosition(self):
         return self.head.getPosition()
-    
+
     def getHeadIndex(self):
         headX, headY = self.head.getPosition()
         return (headY, headX)
 
+
 class SnakeGame():
     def __init__(self, width, height):
-        #arbitrary numbers to signify head, body, and food)
-        #0 for empty space
+        # arbitrary numbers to signify head, body, and food)
+        # 0 for empty space
         self.headVal = 2
         self.bodyVal = 1
         self.foodVal = 7
         self.width = width
         self.height = height
         self.board = np.zeros([height, width], dtype=int)
-        
+
         self.length = 1
-        
+
         startX = width//2
         startY = height//2
-        
+
         self.board[startX, startY] = self.headVal
         self.snake = Snake(startX, startY)
         self.spawnFood()
         self.calcState()
 #        print(self.board)
-    
+
     def spawnFood(self):
-        #spawn food at location not occupied by snake
+        # spawn food at location not occupied by snake
         emptyCells = []
         for index, value in np.ndenumerate(self.board):
             if value != self.bodyVal and value != self.headVal:
                 emptyCells.append(index)
         self.foodIndex = random.choice(emptyCells)
         self.board[self.foodIndex] = self.foodVal
-        
+
     def checkValid(self, direction):
-        #check if move is blocked by wall
+        # check if move is blocked by wall
         newX, newY = self.potentialPosition(direction)
         if newX == -1 or newX == self.width:
             return False
         if newY == -1 or newY == self.height:
             return False
-        #check if move is blocked by snake body
+        # check if move is blocked by snake body
         if self.board[newY, newX] == self.bodyVal:
             return False
         return True
-    
+
     def potentialPosition(self, direction):
         (newX, newY) = self.snake.getHeadPosition()
         if direction == 0:
@@ -117,42 +119,42 @@ class SnakeGame():
         elif direction == 3:
             newX -= 1
         return (newX, newY)
-    
+
     def calcState(self):
-        #state is as follows. 
-        #Is direction blocked by wall or snake?
-        #Is food in this direction?
-        #(top blocked, right blocked, down blocked, left blocked,
+        # state is as follows.
+        # Is direction blocked by wall or snake?
+        # Is food in this direction?
+        # (top blocked, right blocked, down blocked, left blocked,
         # top food, right food, down food, left food)
         self.state = np.zeros(8, dtype=int)
         for i in range(4):
             self.state[i] = not self.checkValid(i)
         self.state[4:] = self.calcFoodDirection()
-    
+
     def calcStateNum(self):
-        ##calculate an integer number for state 
-        ##there will be 2^8 potential states but not all states are reachable
+        # calculate an integer number for state
+        # there will be 2^8 potential states but not all states are reachable
         stateNum = 0
         for i in range(8):
             stateNum += 2**i*self.state[i]
         return stateNum
-    
+
     def calcFoodDirection(self):
-        #food can be 1 or 2 directions eg. right and up
-        #0 is up, 1 is right, 2 is down, 3 is left
+        # food can be 1 or 2 directions eg. right and up
+        # 0 is up, 1 is right, 2 is down, 3 is left
         foodDirections = np.zeros(4, dtype=int)
         dist = np.array(self.foodIndex) - np.array(self.snake.getHeadIndex())
         if dist[0] < 0:
-            #down
+            # down
             foodDirections[0] = 1
         elif dist[0] > 0:
-            #up
+            # up
             foodDirections[2] = 1
         if dist[1] > 0:
-            #right
+            # right
             foodDirections[1] = 1
         elif dist[1] < 0:
-            #left
+            # left
             foodDirections[3] = 1
         return foodDirections
 
@@ -176,36 +178,36 @@ class SnakeGame():
             print('-', end='')
         print()
 #        print(self.board)
-        
+
     def makeMove(self, direction):
         gameOver = False
         if self.checkValid(direction):
-            #set reward if moving in the right direction
+            # set reward if moving in the right direction
             if self.calcFoodDirection()[direction] == 1:
                 reward = 1
             else:
                 reward = 0
             (headX, headY) = self.snake.getHeadPosition()
-            #set old head to body val
+            # set old head to body val
             self.board[headY, headX] = self.bodyVal
-            
-            #check if we got the fruit
+
+            # check if we got the fruit
             potX, potY = self.potentialPosition(direction)
             if self.board[potY, potX] == self.foodVal:
-                #extend the snake
+                # extend the snake
                 self.snake.newHead(potX, potY)
                 self.board[potY, potX] = self.headVal
                 self.spawnFood()
                 self.length += 1
-                #if you want to give a higher reward for getting the fruit, uncomment below
-#                reward = 1
+                # if you want to give a higher reward for getting the fruit, uncomment below
+                # reward = 5
             else:
-                #move the snake
+                # move the snake
                 (oldTailX, oldTailY, newHeadX, newHeadY) = self.snake.move(direction)
                 self.board[oldTailY, oldTailX] = 0
                 self.board[newHeadY, newHeadX] = self.headVal
         else:
-            reward = -2
+            reward = -5
             gameOver = True
         self.calcState()
         return (self.calcStateNum(), reward, gameOver, self.length)
@@ -233,4 +235,3 @@ if __name__ == "__main__":
         else:
             game.display()
             print("Reward:", reward, "Score:", score)
-        